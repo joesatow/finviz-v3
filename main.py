fcc101017c05bc83efc42ba806dcc5318a46d46b
joe <joseph.satow@gmail.com>
2023-09-16 17:25:29 +0000

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from finviz.screener import Screener
+from helper_funcs import getCurrentDirectory, filterTickers
+
+import time
+import sqlite3
+import time
+import os, shutil
+
+print("test")
