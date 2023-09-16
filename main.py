da7c03d4bbbf7e94919a6a719ba5f9aad2a7e3f8
joe <joseph.satow@gmail.com>
2023-09-16 18:14:58 +0000

    test

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 from finviz.screener import Screener
-from helper_funcs import getCurrentDirectory, filterTickers
+import time
 
-import time
-import sqlite3
-import time
-import os, shutil
+# Filters
+bigFilters = ["sh_curvol_o200", "sh_opt_option","sh_price_o100"] # Big filter
+tohFilters = ["sh_curvol_o2000", "sh_opt_option", "sh_price_20to100"] # 20-100 filter
+
+# Screening/scraping finviz process
+big_list = Screener(filters=bigFilters, order="ticker")
+time.sleep(1)
+toh_list = Screener(filters=tohFilters, order="ticker")
 
 print("test")
